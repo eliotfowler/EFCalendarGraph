61b6594bcfb9196544fb70e21b0e36ca6d4c9b56
Eliot Fowler <efowler@cardinalsolutions.com>
2015-07-08 08:59:55 -0400

    adding the custom view and date utilities

---
+++ EFCalendarGraph/EFCalendarGraph.h
@@ -0,0 +1,13 @@
+//
+//  EFCalendarView.h
+//  EFCalendarGraph
+//
+//  Created by Eliot Fowler on 7/6/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EFCalendarView : UIView
+
+@end
---
+++ EFCalendarGraph/EFCalendarGraph.m
@@ -0,0 +1,13 @@
+//
+//  EFCalendarView.m
+//  EFCalendarGraph
+//
+//  Created by Eliot Fowler on 7/6/15.
+//
+//
+
+#import "EFCalendarView.h"
+
+@implementation EFCalendarView
+
+@end
